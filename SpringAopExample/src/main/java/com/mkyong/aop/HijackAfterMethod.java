27fb3e4ea9cd2e513d1c4c7b81b253e5504d4071
najjar <mohamed_najjar@uff.net>
2017-04-19 14:40:16 +0200

    first commit branch

--- SpringAopExample/src/main/java/com/mkyong/aop/HijackAfterMethod.java
+++ SpringAopExample/src/main/java/com/mkyong/aop/HijackAfterMethod.java
@@ -7,6 +7,7 @@ public class HijackAfterMethod implements AfterReturningAdvice {
 	@Override
 	public void afterReturning(Object returnValue, Method method,
 			Object[] args, Object target) throws Throwable {
+		System.out.println();
 		System.out.println("HijackAfterMethod : After method hijacked!");
 	}
 }
